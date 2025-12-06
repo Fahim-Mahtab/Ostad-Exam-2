fdfd3ffde8175e0e3cf155da9306e7b6eb67741a
Fahim Mahtab <fahimmahtab5@gmail.com>
2025-12-06 22:29:26 +0600

    added files

---
+++ lib/ui/home_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        centerTitle: false,
+        title: const Text('Food Recipes'),
+      ),
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Icon(Icons.cake_outlined),
+            title: Text(
+              "This is cake",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            subtitle: Text("this is description"),
+          );
+        },
+      ),
+    );
+  }
+}
